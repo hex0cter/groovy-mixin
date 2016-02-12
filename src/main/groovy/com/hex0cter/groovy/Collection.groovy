5e160fd4be1d46147f10177968970e0b1e2e5e44
Daniel Han <hex0cter@gmail.com>
2016-02-12 13:35:35 +0200

    refactoring done

--- src/main/groovy/com/hex0cter/groovy/Collection.groovy
+++ src/main/groovy/com/hex0cter/groovy/Collection.groovy
@@ -6,13 +6,39 @@ import java.security.SecureRandom
  * Collection mixin
  */
 class Collection {
-    static Object sample(final List self) {
+    static Object sample(final List self, int n = 1) {
+        def indice = (1..self.size()) as ArrayList
+
         SecureRandom random = new SecureRandom()
-        int i = random.nextInt(self.size())
-        self[i]
+        Collections.shuffle(indice, random)
+
+        def retval = []
+        n.times {
+            retval << self[indice[it] - 1]
+        }
+
+        switch (n) {
+            case [0, 1]:
+                return retval[0]
+            default:
+                return retval
+        }
     }
 
-    static boolean isPartOf(final Object object, final List collection) {
-        collection.contains(object)
+    static boolean isPartOf(final def objects, final List collection) {
+        def elements
+        if (objects instanceof List) {
+            elements = objects
+        } else {
+            elements = [objects]
+        }
+
+        for (element in elements) {
+            if (!collection.contains(element)) {
+                return false
+            }
+        }
+
+        true
     }
 }
--- src/test/groovy/com/hex0cter/groovy/test/CollectionSpec.groovy
+++ src/test/groovy/com/hex0cter/groovy/test/CollectionSpec.groovy
@@ -5,15 +5,29 @@ import spock.lang.Specification
 /**
  * CollectionSpec Test
  */
+@SuppressWarnings(['DuplicateNumberLiteral', 'DuplicateListLiteral'])
 class CollectionSpec extends Specification {
     def "take sample from a collection"() {
         expect:
         collection.sample().isPartOf collection
+        collection.sample(3).isPartOf collection
 
         where:
         collection | _
         [1, 2, 3]  | _
-        (1..1000)  | _
+        (10..100)  | _
+        (3..15.9)  | _
+    }
+
+    def 'an element belongs to a collection'() {
+        expect:
+        1.isPartOf collection
+        [1, 2].isPartOf collection
+
+        where:
+        collection | _
+        [1, 2, 3]  | _
+        (1..100)   | _
         (1..15.9)  | _
     }
 }
